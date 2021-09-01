375ba0bccd70445fc6da6df0025a46345a5d0b72
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-01 12:09:21 +0530

    Nested declaration added.

--- IMP_POINTS_SQL_ORDB.sql
+++ IMP_POINTS_SQL_ORDB.sql
@@ -28,3 +28,38 @@ INSERT INTO sells VALUES(
 	(SELECT REF(b) FROM bars b WHERE name='Jim''s'),
 	(SELECT REF(e) FROM beers e WHERE name='Swan'),
 	2.40);
+
+
+-- CREATING NESTED TABLES. 
+-- CREATING THE OBJECT TEMPLATE FOR THE NESTED TABLE.
+CREATE TYPE investment_type AS OBJECT 
+(
+    company REF stock_type,
+    purchasePrice NUMBER (6,2),
+    purchaseDate DATE,
+    quantity NUMBER(6)
+)
+/
+
+-- CREATE THE NESTED TABLE FOR THE INVESTMENTS USING THE ABOVE TEMPLATE (DIFFERNET THAN CREATING A TABLE)
+CREATE TYPE investment_nestedtbl_type AS TABLE OF investment_type
+/
+
+-- CREATING THE PARENT TEMPLATE FOR PARENT TABLE.
+-- CREATING THE OBJECT FOR THE CLIENTS. (REFER TO THE NESTED TABLE CREATED ABOVE)
+CREATE TYPE client_type AS OBJECT 
+(
+    name VARCHAR (40),
+    address adress_type,
+    investment investment_nestedtbl_type
+)
+/
+
+-- CREATING THE PARENT TABLE.
+CREATE TABLE client of client_type 
+(
+    CONSTRAINT client_pk PRIMARY KEY(name)
+) NESTED TABLE investment STORE AS investment_tab
+/
+
+
