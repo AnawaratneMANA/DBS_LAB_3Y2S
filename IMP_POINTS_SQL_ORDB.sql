3a925d5259fc9c8e761f46082fe04cc083e1a21e
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-01 12:03:23 +0530

    Varray Declaration added.

--- IMP_POINTS_SQL_ORDB.sql
+++ IMP_POINTS_SQL_ORDB.sql
@@ -1,3 +1,19 @@
+-- CREATING A VARRAY FOR THE EXCHANGE TRADED.
+CREATE TYPE exchanges_varray as VARRAY(3)
+OF VARCHAR(40)
+/
+
+-- INCLUDE CRATED VARRAY IN A OBJECT BLUEPRINT.
+CREATE TYPE stock_type AS OBJECT 
+(
+    companyName VARCHAR(20),
+    currentPrice NUMBER (6,2),
+    exchanges exchanges_varray,
+    lastDivident NUMBER (4,2),
+    eps NUMBER (4,2)
+)
+/
+
 -- CREATE A TABLE WITH REF
 CREATE TYPE MenuType2 AS OBJECT (
 	bar		REF BarType,
