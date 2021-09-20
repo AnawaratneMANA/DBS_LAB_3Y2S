81c0bcc353c847efbaff8cda018934816f988e03
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-20 16:23:31 +0530

    Printing statement and comments in PL SQL

--- IMP_POINTS_PL_SQL.sql
+++ IMP_POINTS_PL_SQL.sql
@@ -7,3 +7,12 @@ BEGIN
 EXCEPTION
     --Exception handling seciton
 END
+
+/* Comments in PL SQL - This is a PL Multiline Comment */
+-- Comments in PL SQL - This is a PL Single Line Comment
+
+/* Printing Values in PL SQL */
+DECLARE 
+BEGIN
+    DBMS_OUTPUT.PUT_LINE("Printing Message");
+END;
