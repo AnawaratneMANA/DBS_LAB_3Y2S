2bd21fb76ee377e0a87ca12600f82d9c3d0f7092
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-21 15:39:25 +0530

    Value() method example Added.

--- xpath-ms-sql-server.sql
+++ xpath-ms-sql-server.sql
@@ -9,3 +9,8 @@ CREATE TABLE AdminDocs (
     id int PRIMARY KEY,
     xDoc XML (CONTENT myCollection)
 )
+
+/* Extract Scaler values using Value() method */
+SELECT xDoc.value('data((/doc/section[@num = 3]/title)[1]', 'nvarchar(max)')
+FROM AdminDocs
+
